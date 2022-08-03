7f8e06e05f7ed8e44df43511fee69aa4db26f498
alperderman <73118676+alperderman@users.noreply.github.com>
2022-08-03 16:40:17 +0300

    NEW DESIGN

--- assets/js/modal.js
+++
@@ -1,9 +0,0 @@
-function modalOpen(id){
-  $('[data-modal="'+id+'"]').addClass("show");
-  $('body').addClass("overflow-hidden");
-}
-
-function modalClose(id){
-  $('[data-modal="'+id+'"]').removeClass("show");
-  $('body').removeClass("overflow-hidden");
-}
